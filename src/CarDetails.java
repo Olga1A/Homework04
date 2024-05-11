04301ec1fc5e6a6d78d139160181ddd0ca8a33dd
Olga <aleksandrova1olga@gmail.com>
2024-05-11 17:54:22 +0300

    Homework04.

--- src/CarDetails.java
+++ src/CarDetails.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class CarDetails {
+public class CarDetails {
+    public static void main(String[] args) {
+        Car favoriteCar = new Car ();
+        favoriteCar.brand = "Maserati";
+        favoriteCar.model = "Quattroporte";
+        favoriteCar.year = 2023;
+    }
 }
--- src/PercentageCalc.java
+++ src/PercentageCalc.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class PercentageCalc {
+public class PercentageCalc {
+    public static void main(String[] args) {
+
+        int value = 130;
+        double percent = 0.10;
+        System.out.println(130*0.10/100);
+
+        double a = 0.01;
+        double b = 0.13;
+        System.out.println(a < b);
+    }
 }
